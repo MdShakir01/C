df10c5ee087724d69af90e7d0e04623aaad93eaf
MdShakir01 <warkillerking89@gmail.com>
2025-10-14 13:29:36 +0530

    This is next practice question

---
+++ 10_string/4_takeinput_through_user.c
@@ -0,0 +1,25 @@
+//1 step
+/*#include<stdio.h>
+int main(){
+    //agar stdio.h labrary me he inpute lena hai sentance ko to 4 cheez use hota hai jes me 2 safe hai or 2 unsafe
+    //safe wala 1)fgets(), 2)scanf("%[^\n]s").....unsafe 1)getchar() loop, 2)get()
+    char v[50];
+    printf("Enter Your name and age = ");
+    fgets(v,50,stdin);
+    printf("\nHello, %s",v);
+
+    printf("\nEnter your collage name = ");
+    scanf("%[^\n]s",v);
+    printf("\nYour collage name is %s",v);
+
+    //ek character ko agar lena hai to us k leye v 4 datatypw ka use kare gen 2 jes se only one character print hoga or 2 jes se ek he character print hoga but kese v datatype ka (int,float etc)
+    //only one character 1)getchar(), 2)putchar()......dusre data type (int,float) v le rahe hai 1)scanf("%c",..), 2)printf("%c",...)
+    char k;
+    printf("\nEnter only one character : ");
+    k = getchar();
+    printf("\nYour Character is : ");
+    putchar(k);
+
+    return 0;
+}*/
+
